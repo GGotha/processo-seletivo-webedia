456339d4c2ae01902087e112436e04c004992679
Gustavo <snakygamer@gmail.com>
2019-08-15 00:53:28 -0300

    migration

--- src/database/migrations/20190802004820-create-users.js
+++ src/database/migrations/20190802004820-create-users.js
@@ -18,7 +18,7 @@ module.exports = {
         unique: true,
         type: Sequelize.STRING
       },
-      password: {
+      password_hash: {
         allowNull: false,
         type: Sequelize.STRING
       },
